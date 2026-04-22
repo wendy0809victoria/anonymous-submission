b005b296c7a6130413576e7e4a8dc3ad6ea54085
wendy0809victoria <144867902+wendy0809victoria@users.noreply.github.com>
2026-04-22 14:47:35 -0400

    fourth commit

--- main.py
+++ main.py
@@ -11,42 +11,15 @@ import utility
 
 
 if __name__ == '__main__':
-    # import os, torch
-
-    # os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-
-    # torch.backends.cudnn.benchmark = False
-    # torch.backends.cudnn.deterministic = True
-
-    # torch.backends.cuda.matmul.allow_tf32 = False
-    # torch.backends.cudnn.allow_tf32 = False
-
-    # torch.use_deterministic_algorithms(True)
-
     arg_setup = utility.parse_args()
-
-    # ckpt_path = f"{arg_setup.log_dir}/states_epoch_9.pth"
-    # ckpt = torch.load(ckpt_path, map_location="cpu", weights_only=False)
-    # # ---- Python / NumPy ----
-    # pyrandom.setstate(ckpt["python_rng"])
-    # np.random.set_state(ckpt["numpy_rng"])
-    # # ---- Torch CPU ----
-    # torch.set_rng_state(ckpt["rng_state"])
-    # # ---- Torch CUDA (2 GPU) ----
-    # torch.cuda.set_rng_state_all(ckpt["cuda_rng_state"])
     
     ''' model and loaders '''
-    # all_datasets, all_loader, dataloader_train_aug = dms.get_all(seed = 1, batchsize_train = arg_setup.expected_batchsize) # the batchsize here is of on meaning
     all_datasets, all_loader = dms.get_all(seed = 1, batchsize_train = arg_setup.expected_batchsize)
     model = dms.model(num_of_classes = dms.num_of_classes).to(dms.device)
     model.device = dms.device
-    # model = dms_wide.WRN_16_2().to(dms_wide.device)
-    # model.device = dms_wide.device
     
     ''' old train loader'''
-    # train_loader = ph.privatized_loader_old(all_datasets[0], arg_setup.expected_batchsize)
     train_loader = ph.privatized_loader(all_datasets[0], arg_setup.expected_batchsize)
-    # train_loader = all_loader[0]
     
     ''' total image number for batch parameter computation, pub data, from train data'''
     arg_setup.usable_train_data_samples = len(all_datasets[0])
@@ -66,13 +39,8 @@ if __name__ == '__main__':
 
     arg_setup.self_augment = False
     
-    # arg_setup.dataloader_train_aug = dataloader_train_aug
-    
     '''sgd opti'''
     opti = torch.optim.SGD(model.parameters(), lr = arg_setup.lr, momentum = 0.0 ); arg_setup.beta = 0.9
-
-    '''adam opti'''
-    # opti = torch.optim.Adam(model.parameters(), lr = 0.01); arg_setup.beta = 0
     
     ''' function signature '''
     TRAIN_SETUP_LIST = ('epoch', 'device', 'optimizer', 'loss_metric', 'enable_per_grad')
